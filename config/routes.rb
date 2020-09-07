eb41ed2ecf02815b8c054a002ae2792f940d45a7
takeuchikanji <wands.ur@ezweb.ne.jp>
2020-09-07 18:35:31 +0900

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+
   root "comics#index"
-  get 'comics/index'
 end
