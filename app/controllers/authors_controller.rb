751106cc0727ba8feaf4696d5410ff519f1b9201
takeuchikanji <wands.ur@ezweb.ne.jp>
2020-11-07 16:00:38 +0900

    11/7 authors_update_fix

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -22,14 +22,6 @@ class AuthorsController < ApplicationController
     if @author.new_record?
       @author.save
     end
-    # binding.pry
-    # @author = Author.create(author_params)
-    # @author = Author.new(author_params)       ##ここでauthorを代入したい
-    # binding.pry
-    # @author = author.id
-    # @author.save
-    
-    # binding.pry
     comic = Comic.new(comic_params)
     comic.save!
     redirect_to root_path
@@ -42,15 +34,25 @@ class AuthorsController < ApplicationController
 
   def update
     @author = Author.find(params[:id])        ##作者取得
-    @comic = Comic.find_by(name: params[:format])     ##作者の作品のうち対応する作品を取得
-    if @author.name == author_params[:name]     ##作者名変更しなかったらtrue       ＜＜＜＜＜＜＜＜＜更新できてない動いてない＞＞＞＞＞＞＞＞
-      @author.save          ##作者名以外をaccept~で更新
+    @comic = Comic.find_by(name: params[:format])     ##作者の作品のうち対応する作品を取得    この行、要らないかもなんかこの@comicじゃ取れてこれず(@comic=nil)で結局下で再度@comic定義してる。。。
+    if @author.name == author_params[:name]     ##作者名変更しなかったらtrue
+      @author.save
+      @comic = Comic.find(params.require(:author).require(:comic).require(:id))
+      @comic.update(update_author_comic_params)
     else                        ##作者名を変更したときの処理
-      @author_db_find = Author.where(name: author_params[:name]).first_or_initialize    ##再度、変更した作者がDBないにあるかどうか探す
+      @author_db_find = Author.where(name: author_params[:name]).first_or_initialize    ##再度、変更した作者がDBないにあるかどうか探す(変更後の作者をこの変数に入れて、@author（変更前の作者）とは別にしている)
       if @author_db_find.new_record?                         ##もし、なければauthorの新しいレコードを作成
         @author_db_find.save            ##諸々変更を保存
+        if @author.comics.length == 1      ##対象の作者の作品がラスト１だったら、作者を削除   こっちでも変更前の作者が作品０で作者だけ残らないようにする
+          @author.comics.each do |comic|    ##削除するが、この記述(reloadまで)しておかないと、モデルに書いているdependent: :destroyによって作品ごと削除してしまう
+            comic.author = nil
+            @author_db_find.comics << comic
+          end
+          @author.reload
+          @author.destroy
+        end
         @comic = Comic.find(params.require(:author).require(:comic).require(:id))
-        @comic.update(update_comic_params)
+        @comic.update(update_author_db_find_comic_params)
       else                            ##変更した作者名がすでにDBにあればこっち
         if @author.comics.length == 1      ##対象の作者の作品がラスト１だったら、作者を削除
           @author.comics.each do |comic|    ##削除するが、この記述(reloadまで)しておかないと、モデルに書いているdependent: :destroyによって作品ごと削除してしまう
@@ -59,10 +61,10 @@ class AuthorsController < ApplicationController
           end
           @author.reload
           @author.destroy
-        else      ####対象の作者の作品が2つ以上残っている
-          @comic = Comic.find(params.require(:author).require(:comic).require(:id))
-          @comic.update(update_comic_params)
-        end
+        end      
+        @comic = Comic.find(params.require(:author).require(:comic).require(:id))   ##この2行を上のifでelseの時のみの挙動にしていたが、1つ削除するときも、コミック情報自体は更新する必要があるため、インデント左に上げてどっちでも保存するようにした
+        @comic.update(update_author_db_find_comic_params)
+        
       end
     end
 
@@ -94,7 +96,11 @@ class AuthorsController < ApplicationController
     params.require(:author).require(:comics_attributes).require("0").permit(:name, :image, :number_of_books, :summary, :review, :booknumber_id, genre_ids: []).merge(author_id: @author.id)
   end
 
-  def update_comic_params     ##accept~使わず@comicでfield_forをした
+  def update_author_comic_params     ##元の作者変更しなかった場合こっち（変更前の作者idをmarge）
+    params.require(:author).require(:comic).permit(:name, :image, :number_of_books, :summary, :review, :booknumber_id, genre_ids: []).merge(author_id: @author.id)
+  end
+
+  def update_author_db_find_comic_params     ##元の作者から変更した場合こっち（変更後の作者idをmarge）
     params.require(:author).require(:comic).permit(:name, :image, :number_of_books, :summary, :review, :booknumber_id, genre_ids: []).merge(author_id: @author_db_find.id)
   end
 end
