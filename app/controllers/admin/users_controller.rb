be7012cb95e7483914fa580c476b7a6d509336a7
takeuchikanji <wands.ur@ezweb.ne.jp>
2020-11-15 12:42:12 +0900

    11/15 admin-display_basics

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,6 +5,11 @@ class Admin::UsersController < ApplicationController
     @users = User.all.order("created_at DESC")
     @comics = Comic.all.order("created_at DESC")
     @authors = Author.all.order("created_at DESC")
+    @genres = Genre.all.order("created_at DESC")
+    @user = User.order(updated_at: :desc).limit(1)
+    @comic = Comic.order(updated_at: :desc).limit(1)
+    @author = Author.order(updated_at: :desc).limit(1)
+    @genre = Genre.order(updated_at: :desc).limit(1)
   end
 
   private
