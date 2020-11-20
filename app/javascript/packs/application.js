f4c31ea2361b2eec6947d6259f6ecef00dadb43c
takeuchikanji <wands.ur@ezweb.ne.jp>
2020-11-20 14:34:07 +0900

    11/20 flash-meesage_fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,10 @@ require('drawer_menu')
 require('comment')
 require('review_hide.js')
 
+$(function(){
+  setTimeout("$('.Notification').fadeOut('slow')", 2000);
+});
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
